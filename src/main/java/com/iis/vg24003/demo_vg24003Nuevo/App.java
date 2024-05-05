ab77d670dc1d27e029999ae7893fbb4df28e9dd9
BrayanBV <vg24003@ues.edu.sv>
2024-05-04 21:43:47 -0600

    Actual

---
+++ src/main/java/com/iis/vg24003/demo_vg24003Nuevo/App.java
@@ -0,0 +1,13 @@
+package com.iis.vg24003.demo_vg24003Nuevo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
